b6a46ea52ff044f1ad267c672aaeb9cfca6470d9
mrtyagi07 <vaibhavtyagi438@gmail.com>
2024-10-13 19:44:48 +0530

    chore: Add Analytics component and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
 				)}
 			>
 				{children}
+				<Analytics />
 			</body>
 		</html>
 	);
